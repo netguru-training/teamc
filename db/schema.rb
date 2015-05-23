71f158289bb2be7449b56eb381143c61579b9d41
ppawlik <pawlik.peter@gmail.com>
2015-05-23 11:51:31 +0200

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523085436) do
+ActiveRecord::Schema.define(version: 20150523094148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 20150523085436) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "board_games", force: :cascade do |t|
+    t.string   "name",           null: false
+    t.text     "description"
+    t.integer  "min_players"
+    t.integer  "max_players"
+    t.integer  "min_age"
+    t.string   "img_small__url"
+    t.string   "img_full_url"
+    t.integer  "object_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name",        null: false
