ec843f3464f0f36e4e2491682daa69bac05f1549
ppawlik <pawlik.peter@gmail.com>
2015-05-23 16:31:41 +0200

    Basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,15 @@ user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
+
+4.times do |i|
+  r = Room.create( city: "New York biatch!", street: "Smith Street", room_number: i )
+  u = User.create( email: "spiderman" + i.to_s + "@example.com", password: "password", password_confirmation: "password" )
+  Event.create(
+    name: "Event " + i.to_s,
+    description: "Some very convincing description here!",
+    datetime: i.minutes.from_now,
+    room_id: r.id,
+    owner_id: u.id
+    )
+end
