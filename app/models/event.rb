eaa35f52cf20cd345bd269f53a0bdd58d54e21dc
ppawlik <pawlik.peter@gmail.com>
2015-05-23 12:12:37 +0200

    Many to many between user and event through participants

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
 	has_many :event_games
 	has_many :board_games, :through => :event_games
+
+	has_many :participants
+  	has_many :users, :through => :participants
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :participants
+  has_many :events, :through => :participants
 end
