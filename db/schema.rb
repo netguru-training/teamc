59dc49860ff8fcf53c501d5192ed9216ce4452a5
ppawlik <pawlik.peter@gmail.com>
2015-05-23 10:35:44 +0200

    Event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523071711) do
+ActiveRecord::Schema.define(version: 20150523082617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.string   "place",       null: false
+    t.datetime "datetime",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
