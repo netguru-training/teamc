c1b3df97571191dfd900980f3be1b84a738dfded
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-23 13:32:40 +0200

    Display search form in event index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,8 @@ class EventsController < ApplicationController
   expose(:event)
 
   def find_events
-    if params[:find_near].present?
-      rooms = Room.near(params[:find_near], 20, units: :km)
+    if params[:search].present?
+      rooms = Room.near(params[:search][:find_near], 20, units: :km)
       Event.where(room_id: rooms.map(&:id))
     else
       Event.all
