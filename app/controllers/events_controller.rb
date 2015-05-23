4811cb422fb12030f69b45f5d5c89eaeca8e96f7
Luke Pawlik <lukepawlik@gmail.com>
2015-05-23 14:55:47 +0200

    Add new, edit to event_controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,18 @@ class EventsController < ApplicationController
   expose(:events) { find_events }
   expose(:event)
 
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
   def find_events
     if params[:search].present?
       rooms = Room.near(params[:search][:find_near], 20, units: :km)
