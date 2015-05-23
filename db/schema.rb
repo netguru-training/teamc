126d17af53bc531e3e64d535e2b530c9a34937fd
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-23 10:42:56 +0200

    Move navigation_links to right

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150523082617) do
-=======
-ActiveRecord::Schema.define(version: 20150523081730) do
->>>>>>> e546a6595a18466b63287dbfc2d426a52b2b6c93
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-  create_table "events", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.text     "description"
-    t.string   "place",       null: false
-    t.datetime "datetime",    null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-=======
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
@@ -64,7 +49,15 @@ ActiveRecord::Schema.define(version: 20150523081730) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
->>>>>>> e546a6595a18466b63287dbfc2d426a52b2b6c93
+  create_table "events", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.string   "place",       null: false
+    t.datetime "datetime",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
