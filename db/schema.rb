f46a4e8d07bff7f9398c7ad282ae348dcf1f6678
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-23 16:30:11 +0200

    Add filed private to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523104129) do
+ActiveRecord::Schema.define(version: 20150523142548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,13 +71,14 @@ ActiveRecord::Schema.define(version: 20150523104129) do
   add_index "event_games", ["event_id"], name: "index_event_games_on_event_id", using: :btree
 
   create_table "events", force: :cascade do |t|
-    t.string   "name",        null: false
+    t.string   "name",                        null: false
     t.text     "description"
-    t.datetime "datetime",    null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "datetime",                    null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "room_id"
     t.integer  "owner_id"
+    t.boolean  "private",     default: false
   end
 
   create_table "participants", force: :cascade do |t|
