1d0b1d70283b3cc9f80a596ce991a403e72bc784
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-23 12:10:12 +0200

    Validate presence of city and street in Game and assigns its longitude and latitude

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,11 @@
 class Room < ActiveRecord::Base
+  geocoded_by :address
+  after_validation :geocode
+
+  validates :city, presence: true
+  validates :street, presence: true
+
+  def address
+    "#{city}, #{street}"
+  end
 end
