88e24567362314e110fa4b18b977f7eeaadba523
Luke Pawlik <lukepawlik@gmail.com>
2015-05-24 16:20:44 +0200

    Add spec #owner? for event model.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 describe Event, type: :model do
   it { validate_uniqueness_of(:name) }
+
+  describe '#owner?' do
+    let(:event) { create(:event) }
+
+    context 'is owner' do
+      let(:user) { event.owner }
+
+      it "returns true" do
+        expect(event.owner?(user)).to eq true
+      end
+
+    end
+
+    context 'is not owner' do
+      let(:user) {create(:user)}
+
+      it "returns false" do
+        expect(event.owner?(user)).to eq false
+      end
+
+    end
+  end
 end
