cfd89f3f54888a36deb8b3ae95ace44aedf199e3
Luke Pawlik <lukepawlik@gmail.com>
2015-05-24 10:29:07 +0200

    Add event_notificaton in UserMailer.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'notification@boardgamesevents.com'
+
+  def event_notification(user, event)
+    @user = user
+    @event = event
+    mail (to: @user.email, subject: "#{@event.name} is coming!")
+  end
 end
