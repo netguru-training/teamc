df94f92f754b36b17f63dd7ae548b1e1fcc7adba
Luke Pawlik <lukepawlik@gmail.com>
2015-05-24 14:46:04 +0200

    Remove unnecessary comments.

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,20 +1,6 @@
 ActiveAdmin.register Event do
 
 permit_params :name, :description, :room_id, :datetime, board_game_ids: []
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-
 
   form do |f|
     f.inputs "Event" do
