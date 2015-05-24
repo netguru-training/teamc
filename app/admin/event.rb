b2abfda8308628cdf16bd783f2da90680a0bde80
Luke Pawlik <lukepawlik@gmail.com>
2015-05-24 15:02:27 +0200

    Admin can now add participants to event (ActiveAdmin).

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Event do
 
-permit_params :name, :description, :room_id, :datetime, board_game_ids: []
+permit_params :name, :description, :room_id, :datetime, board_game_ids: [], user_ids: []
 
   form do |f|
     f.inputs "Event" do
@@ -12,6 +12,10 @@ permit_params :name, :description, :room_id, :datetime, board_game_ids: []
 
     f.inputs "Games" do
       f.input :board_game_ids, as: :select, input_html: { multiple: true }, collection: ( BoardGame.all )
+    end
+
+    f.inputs "Participants" do
+      f.input :user_ids, as: :select, input_html: { multiple: true }, collection: ( User.all )
     end
 
     f.actions
@@ -27,6 +31,13 @@ permit_params :name, :description, :room_id, :datetime, board_game_ids: []
         event.board_games.each do |game|
           div do
             simple_format game.name
+          end
+        end
+      end
+      row :user_list do
+        event.users.each do |user|
+          div do
+            simple_format user.name
           end
         end
       end
