f5735ba658b16a9cfbbea61c88a6b2b7390ce98c
ppawlik <pawlik.peter@gmail.com>
2015-05-24 11:20:55 +0200

    User can unjoin previously joined event.

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -6,10 +6,14 @@ class ParticipantsController < ApplicationController
   def create
     event.users << current_user
     event.save
+    flash[:success] = "Joined #{event.name} event"
     redirect_to event_url(event.id)
   end
 
   def destroy
-
+    event.users.delete(params[:id])
+    event.save
+    flash[:info] = "Unjoined #{event.name} event"
+    redirect_to event_url(event.id)
   end
 end
